1c5850b28fc7d72b54149c821f7a87c6b3fd48a5
amansahu22 <amansahu2019@iiitkottayam.ac.in>
2021-11-06 02:49:27 +0530

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Header from "./Components/LandingPage/Header";
+import Main from "./Components/LandingPage/Main";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Router>
+      {/* {cartIsShown && <Cart onClose={cartHideHandler} />} */}
+      <Header />
+      <Main />
+      {/* <Routes> */}
+      {/* <Route path='/'>
+          <Main />
+        </Route>
+        <Route path='home'>
+          <Main />
+        </Route> */}
+      {/* <Route path='/form'>
+          <Form/>
+        </Route> */}
+      {/* </Routes> */}
+    </Router>
   );
 }
 
